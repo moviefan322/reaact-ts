683d295ecc6a009fdd0fc2ff4871314db8545340
moviefan322 <philman202@yahoo.com>
2023-06-02 15:30:53 -0400

    adds delete todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import NewTodo from "./components/NewTodo";
 import { Todo } from "./todo.model";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>();
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text }]);
+    setTodos((prev) => [...prev, { id: Math.random().toString(), text }]);
   };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== todoId));
+  };
+
   return (
     <div className="App">
       <NewTodo setTodos={todoAddHandler} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleteTodo={todoDeleteHandler} />
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,13 +3,17 @@ import { Todo } from "../todo.model";
 
 interface TodoListProps {
   todos: Todo[];
+  deleteTodo: (todoId: string) => void;
 }
 
-function TodoList({ todos }: TodoListProps): JSX.Element {
+function TodoList({ todos, deleteTodo }: TodoListProps): JSX.Element {
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={deleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>
       ))}
     </ul>
   );
