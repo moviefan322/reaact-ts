be0f6766d613a854099c4b972f201e59768f0cc3
moviefan322 <philman202@yahoo.com>
2023-06-02 14:53:33 -0400

    renders simple todo list

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface Todo {
+  id: string;
+  text: string[];
+}
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+function TodoList({ todos }: TodoListProps): JSX.Element {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
